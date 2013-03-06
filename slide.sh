7c22969ec2a5494a39d0207b19b5d92b87d893b6
Ryan Uber <ru@ryanuber.com>
2013-03-06 09:29:46 -0800

    Removing dependency on which and substituting in the bash builtin 'type'. Moving the tput command finder inside of the function so there are no variables or statements polluting global namespace.

--- slide.sh
+++ slide.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-declare -r TPUT=$(which tput)
-
-[ -n "$TPUT" -a -c /dev/tty ] || exit 1
-
 function slide() {
+    local -r TPUT=$(type -p tput)
+    [ -x "$TPUT" ] || exit 1
     local -r IFS=''
     local -r MESSAGE=${1:-Press return for next slide}
     local -ri COLS=$($TPUT cols)
@@ -20,8 +18,7 @@ function slide() {
             until [ "$KEY" == 'q' -o "$KEY" = '' ]; do
                 read -s -n1 KEY < /dev/tty
             done
-            [ "$KEY" == 'q' ] && exit 0
-            KEY=1
+            [ "$KEY" == 'q' ] && exit 0 || KEY=1
             continue
         elif [ "$LINE" == '!!center' ]; then
             CENTER=1
